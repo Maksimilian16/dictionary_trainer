2423957f90be921ba341acc82c33ae412120c072
Maksimilian16 <buskomaksym08@gmail.com>
2022-11-25 01:38:12 +0200

    right

--- 1.py
+++ 1.py
@@ -1,38 +1,47 @@
 import json
 import random
-booll = True
 with open('en-uk.json', 'r', encoding='utf-8') as f:
     text = json.load(f)
-with open('uk-en.json', 'r', encoding='utf-8') as f:
-    text1 = json.load(f)
-while booll:
-    fq = input("pick mode:\na. translate Eng-Ukr\nb. translate Ukr-Eng\nc. add word\nd. game\n")
-    if fq == "a":
-        translate_uk = input("print your word: ")
-        if translate_uk in text:
-            print(text.get(translate_uk))
-        else:
-            print(text)
+text1 = {v: k for k, v in text.items()}
+rand = random.randint(0, len(text)-1)
+def asking():
+    fq = input("pick mode:\na. translate En-Ua\nb. translate Ua-En\nc. add word\nd. game\n")
+    xy = input("print ur word: ")
     if fq == "b":
-        translate_eng = input("print your word: ")
-        if translate_eng in text1:
-            print(text1.get(translate_eng))
+        translate_EnToUk(xy)
+    if fq == "a":
+        translate_ukToEn(xy)
     if fq == "c":
-        e = input("print eng word")
-        u = input("print uk word")
-        eu = {e: u}
-        text.update(eu)
-        with open('en-uk.json', 'w') as f:
-            json.dump(text, f)
-        ue = {u: e}
-        text1.update(ue)
-        with open('uk-en.json', 'w') as f:
-            json.dump(text1, f)
+        create_word()
     if fq == "d":
-        hq = random.choice(list(text.values()))
-        print("print translate of word:")
-        gq = input(hq)
-        if text.get(gq) == hq:
-            print("You won")
-        else:
-            print("you lost")
+        game()
+
+
+def translate_ukToEn(ask):
+    if ask in text:
+        print(text[ask])
+
+
+def translate_EnToUk(ask):
+    if ask in text1:
+        print(text1[ask])
+
+
+def create_word():
+    e = input("print eng word")
+    u = input("print uk word")
+    eu = {e: u}
+    text.update(eu)
+    with open('en-uk.json', 'w') as f:
+        json.dump(text, f)
+
+
+def game():
+    xyz = random.choice(list(text.values()))
+    xyz1 = text.get(xyz)
+    ask_game = input(xyz)
+    if ask_game == xyz1:
+        print("You won")
+    else:
+        print("you lost")
+asking()
